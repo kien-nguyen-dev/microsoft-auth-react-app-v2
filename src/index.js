fd431dfb3152508152e54b16b80535562c7bf7a9
Nguyen Trung Kien <kiennt0x@gmail.com>
2023-12-09 01:04:11 +0700

    initialize source code

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./styles/theme";
+
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+import { PublicClientApplication, EventType } from "@azure/msal-browser";
+
+const pca = new PublicClientApplication({
+  auth: {
+    clientId: "ecbc0da6-02d3-47ae-b345-f899cef8506d",
+    authority:
+      "https://login.microsoftonline.com/common",
+    redirectUri: "/",
+    postLogoutRedirectUri: "/",
+    navigateToLoginRequestUrl: false,
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPII) => {
+        console.log(message);
+      },
+      logLevel: "Verbose",
+    },
+  },
+});
+
+pca.addEventCallback((event) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS) {
+    pca.setActiveAccount(event.payload.account);
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App msalInstance={pca} />{" "}
+      </ThemeProvider>{" "}
+    </BrowserRouter>{" "}
+  </React.StrictMode>
+);
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,46 @@
+import { ProfileData } from "../components/ProfileData";
+import { useMsalAuthentication } from "@azure/msal-react";
+import { InteractionType, BrowserAuthError } from "@azure/msal-browser";
+import { useState, useEffect } from "react";
+import { fetchData } from "../fetch";
+
+export const Profile = () => {
+    const { result, error, login } = useMsalAuthentication(InteractionType.Popup, {
+        scopes: ["user.read"]
+    });
+    console.log(result)
+
+    const [graphData, setGraphData] = useState(null);
+
+    useEffect(() => {
+        if (!!graphData) {
+            return
+        }
+
+        if (!!error) {
+            if (error instanceof BrowserAuthError) {
+                login(InteractionType.Redirect, {
+                    scopes: ["user.read"]
+                })
+            }
+            console.log(error);
+        }
+
+        if (result) {
+            fetchData("https://graph.microsoft.com/v1.0/me", result.accessToken)
+                .then(data => console.log("data", data))
+                .catch(error => console.log(error));
+        }
+    }, [error, result, graphData, login]);
+
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <>
+            {graphData ? <ProfileData graphData={graphData} /> : null}
+        </>
+    )
+}
